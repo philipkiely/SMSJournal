bc3fb99b80638abbbf4b6de0ce1caee1e9a62d9f
Philip Kiely <kielyphi@grinnell.edu>
2019-02-03 16:59:40 -0500

    html core

--- SMSJournal/core/urls.py
+++ SMSJournal/core/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
+    path('license/', views.license, name='license'),
 ]
--- SMSJournal/core/views.py
+++ SMSJournal/core/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+# Basic Renders
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def terms(request):
+    return render(request, 'terms.html')
+
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+
+def license(request):
+    return render(request, 'license.html')
