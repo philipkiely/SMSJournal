b4288fae7db235cc9223180d356236d42e7860c9
Philip Kiely <kielyphi@grinnell.edu>
2019-03-11 12:11:17 +0100

    works with new OAuth test

--- SMSJournal/users/urls.py
+++ SMSJournal/users/urls.py
@@ -1,11 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from django.conf import settings
-from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     path('check_code/', views.check_code, name='check_code'),
-    path('', include('social_django.urls', namespace='social_auth')),
-    path('logout/', auth_views.LogoutView, {'next_page': settings.LOGOUT_REDIRECT_URL},
-         name='logout'),
 ]
